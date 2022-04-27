d430f399e56daad0be68b898204e942cc7a7fa64
kholil akhmad <kholilakhmad95@gmail.com>
2022-04-27 14:38:50 +0700

    test add reimburse v2

--- cypress/integration/ESS-UMKM/reimburse/addReimburse.spec.js
+++ cypress/integration/ESS-UMKM/reimburse/addReimburse.spec.js
@@ -1,5 +1,6 @@
 describe('Menu reimburse', () => {
     beforeEach ( () => {
+
         // Cypress starts out with a blank slate for each test
         // so we must tell it to visit our website with the `cy.visit()` command.
         // Since we want to visit the same URL at the start of all our tests,
@@ -13,21 +14,22 @@ describe('Menu reimburse', () => {
     it ( 'Add reimburse ', function () {
         const reason ='Test Add Reimburse'
         const date = '27-04-2022'
-        const amount = '500000'
-        cy.xpath('//a[@id=\'Menu-Penggantian\']').click()
-        cy.xpath('//a[@id=\'Menu-Penggantian-Pengajuan\']').click()
+        const amount = 500000
+
+        cy.get('[id="Menu-Penggantian"]').click()
+        cy.get('[id="Menu-Penggantian-Pengajuan"]').click()
         cy.url().should('contain', 'https://dev-ess-umkm.linovhr.com/reimburse/request/reimburse-request')
         cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-list[1]/div[1]/div[1]/div[2]/div[1]/btn-request[1]/button[1]').click()
         cy.url().should('contain', 'https://dev-ess-umkm.linovhr.com/reimburse/request/reimburse-request/add')
         cy.xpath('//span[contains(text(),\'Pilih Jenis\')]').click()
         cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-add[1]/div[1]/div[2]/div[1]/div[1]/div[1]/app-lov[1]/p-dropdown[1]/div[1]/div[3]/div[2]/ul[1]/p-dropdownitem[2]/li[1]').click()
             .should('contain', 'Medical')
-        cy.xpath('//textarea[@id=\'reason\']').type(`${reason}`)
-        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-add[1]/div[1]/div[2]/div[2]/div[1]/div[1]/p-calendar[1]/span[1]/input[1]')
-            .type('04-04-2022')
+        cy.get('[id="reason"]').type(`${reason}`).should('have.value', reason)
+        cy.get('[id="date"]').type('04-04-2022')
         cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-add[1]/div[1]/div[2]/div[4]').click({ force: true })
-        cy.xpath('//p-inputnumber[@id=\'amount\']').type(`${amount}`)
-        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-add[1]/div[1]/div[1]/div[2]/div[1]/btn-save[1]/button[1]').click()
+        cy.get('[id="amount"]').type(`${amount}`)
+        cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-add[1]/div[1]/div[1]/div[2]/div[1]/btn-save[1]/button[1]')
+            .click()
         cy.xpath('//body/app-root[1]/app-main[1]/div[1]/div[2]/div[1]/app-reimburse-reimburse-request-list[1]/div[1]/p-table[1]/div[1]/div[1]/div[1]/app-search[1]/div[1]/div[1]/div[1]/input[1]')
             .type(`${reason}{enter}`)
     } )
